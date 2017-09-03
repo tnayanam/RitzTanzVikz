a3eb3a8a24ec9475cfcde38ddc4a5607c21584e0
tnayanam <nayatanuj@gmail.com>
2017-09-03 11:50:39 -0400

    added rewards counter

---
+++ Bridge/Controllers/RewardsController.cs
@@ -0,0 +1,26 @@
+﻿using Bridge.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Bridge.Controllers
+{
+    public class RewardsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public RewardsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Rewards
+        public ActionResult ShowRewards()
+        {
+            var candidateId = User.Identity.GetUserId();
+            var referrerCount = _context.Referrals.Where(r => (r.CandidateId == candidateId) && (!string.IsNullOrEmpty(r.ReferrerId))).Count();
+            ViewBag.ReferrerCount = 10 - referrerCount;
+            return View();
+        }
+    }
+}
